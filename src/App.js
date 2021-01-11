1ed71d5f83b8c6e76a7a95f07923d9c6ecedfae7
muhammadridwan47 <rolilaz47@gmail.com>
2021-01-12 01:34:25 +0700

    make splash screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Router from "./router"
 import { Loading } from './components';
 import {Provider,useSelector} from 'react-redux';
 import store from './redux';
 import FlashMessage from "react-native-flash-message";
+import SplashScreen from 'react-native-splash-screen';
 const MainApp = () => {
   const {loading} = useSelector(state => state);
+  useEffect(() => {
+    SplashScreen.hide();
+  },[])
   return (
     <>
         <NavigationContainer>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -35,7 +35,6 @@ export default function Profile({navigation}) {
           setData('')
         })
     }
-
     const notFound = () => {
       showMessage({
         message: "Data Not Found!",
