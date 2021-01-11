0f01fb282e68b3b653312e29fc7eac00d13f9189
muhammadridwan47 <rolilaz47@gmail.com>
2021-01-11 16:02:49 +0700

    setup folder assets,components,pages,router,and utils

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <>
+     
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
